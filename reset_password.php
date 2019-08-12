2366f5771a74a5351a749b84516cea7727b35e54
namithaodackal1996@gmail.com <namithaodackal1996@gmail.com>
2019-08-12 23:36:11 +1200

    reset password
    this page is used to reset password by new password if only the user already signed to the website.

---
+++ reset_password.php
@@ -0,0 +1,96 @@
+<?php
+include("config.php");
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // username and password sent from form 
+
+    $username =  $_POST['username'];
+    $old_password =  $_POST['old_password'];
+    $new_password =  $_POST['new_password'];
+
+
+    $FetchStatement = "SELECT id FROM users WHERE username = '$username' and password = '$password'";
+    $stmt = $conn->prepare($FetchStatement);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+
+    $count = $result->num_rows;
+    // print_r($result);
+
+
+    if ($count < 1) {
+        $FetchStatement = "UPDATE users SET password = '$new_password' WHERE username='$username'";
+        $stmt = $conn->prepare($FetchStatement);
+        $stmt->execute();
+        header("location: login.php");
+    } else {
+        $error = "Your Login Name or Old Password is invalid";
+    }
+}
+?>
+<html>
+
+<head>
+    <title>Login Page</title>
+
+
+
+</head>
+
+<body bgcolor="#FFFFFF">
+
+
+    <form action="" method="post" class="LoginForm">
+        <div>Username</div>
+        <input type="text" name="username" class="box" />
+        <div>Current Password</div>
+        <input type="password" name="old_password" class="box" /><br />
+        <div>New Password</div>
+        <input type="password" name="new_password" class="box" /><br /><br />
+        <input type="submit" value=" Submit " /><br />
+        <p>
+            Dont have an account? <a href="registration.php">Sign up</a>
+        </p>
+
+    </form>
+
+
+    <div style="font-size:11px; color:#cc0000; margin-top:10px"><?php if (isset($error)) {
+                                                                    echo
+                                                                        $error;
+                                                                } ?></div>
+
+
+
+
+</body>
+<style>
+    body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        width: 100vw;
+        height: 100vh;
+    }
+
+    .LoginForm {
+        display: flex;
+        flex-direction: column;
+        width: 200px;
+        justify-content: flex-start;
+    }
+
+    .LoginForm input {
+        height: 40px;
+        width: 200px;
+        border: 1px solid #777;
+        border-radius: 40px;
+        padding: 0px 10px;
+        margin: 5px;
+    }
+</style>
+
+</html>
